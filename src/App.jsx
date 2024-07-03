089917dd57395cd7c44fd6925c36dd05b6e7293b
opeyemi <opeyemiodunayo2018@gmail.com>
2024-07-02 23:05:02 -0700

    i created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div className='text-3xl'>
-      
-      App</div>
+    <BrowserRouter >
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/sign-in' element={<Signin/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 

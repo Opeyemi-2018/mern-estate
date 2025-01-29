fbd589a57db94e6fa6a24a21d44024b4e3ba913a
opeyemi <opeyemiodunayo2018@gmail.com>
2025-01-29 13:14:00 +0100

    i added property search page

--- client/src/component/Hero.jsx
+++ client/src/component/Hero.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import designOne from "../assets/images/design-1.jpg";
 import designTwo from "../assets/images/design-2.jpg";
 import HeroImage from "../assets/images/hero-image.jpeg";
+import SearchProperty from "./SearchProperty";
 
 const HeroContent = [
   {
@@ -39,10 +40,11 @@ const Hero = () => {
 
   return (
     <div
-      className="relative w-full h-[400px] flex items-center justify-center"
+      className="relative w-full h-[400px] flex items-center flex-col gap-10 justify-center transition-all duration-1000 ease-in-out"
       style={{
-        background: `linear-gradient(to bottom, rgba(20, 45, 75, 1) 0%, rgba(18, 50, 80, 0.8) 60%, rgba(15, 55, 85, 0.3) 90%), url(${HeroContent[index].image}) center/cover no-repeat`,
-        transition: "background 1s ease-in-out",
+        background: `linear-gradient(to bottom, rgba(20, 45, 75, 1) 0%, rgba(18, 50, 80, 0.8) 60%, rgba(15, 55, 85, 0.3) 90%), 
+                 url(${HeroContent[index].image}) center/cover no-repeat`,
+        backdropFilter: "blur(5px)",
       }}
     >
       {/* <div className="flex items-center justify-center py-28 px-3"> */}
@@ -52,15 +54,16 @@ const Hero = () => {
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
         transition={{ duration: 1 }}
-        className="text-center text-white max-w-6xl px-4"
+        className="text-center text-white "
       >
         <h1 className="text-3xl lg:text-6xl font-bold">
           {HeroContent[index].title}
         </h1>
         <p className="text-lg mt-4">{HeroContent[index].description}</p>
       </motion.div>
+
+      <SearchProperty />
     </div>
-    // </div>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -175,36 +175,3 @@ export default function Home({ showNav }) {
     </div>
   );
 }
-
-{
-  /* <div>
-              <form className="bg-white p-4 rounded-md ">
-                <div className="flex items-center justify-between mb-5">
-                  <input
-                    type="text"
-                    className="p-1  bg-gray-100 outline-none border-r-2"
-                    placeholder="apartment type"
-                  />
-                  <input
-                    type="text"
-                    className="p-1 border-none outline-none bg-gray-100"
-                    placeholder="location"
-                  />
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <div className="flex gap-2">
-                    <button className="bg-[#DBB65D] w-[60px] py-[2px] px-2 rounded-sm">
-                      Buy
-                    </button>
-                    <button className="bg-gray-200 w-[60px] py-[2px] px-2 rounded-sm">
-                      Rent
-                    </button>
-                  </div>
-                  <button className="bg-[#1E2128] py-[2px] px-3 text-white rounded-sm">
-                    Search
-                  </button>
-                </div>
-              </form>
-            </div> */
-}

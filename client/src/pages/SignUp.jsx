7abbd9e08e58df8df9fe9efc77ea633c361ac66f
Opeyemi odunayo <127036369+Opeyemi-2018@users.noreply.github.com>
2025-04-21 14:28:16 +0100

    remove the google auth

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -239,7 +239,7 @@ const SignUp = () => {
               Sign Up
             </button>
 
-            <OAuth />
+            {/* <OAuth /> */}
           </form>
           <div className="flex gap-2 mt-5">
             <p>Have an account?</p>

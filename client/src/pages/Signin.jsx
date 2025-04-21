172a86527371334e11d06a655a520a9f730d1b7a
Opeyemi odunayo <127036369+Opeyemi-2018@users.noreply.github.com>
2025-04-21 14:37:25 +0100

    removed google OAuth

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -141,7 +141,7 @@ const Signin = () => {
             >
               Sign in
             </button>
-            <OAuth />
+            {/* <OAuth /> */}
           </form>
           <div className="flex gap-2 mt-5">
             <p>Dont Have an account</p>
